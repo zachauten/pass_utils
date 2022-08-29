e51ac574360eb8b759739bb5fc421ab5128e6986
Zach Auten <zachauten@gmail.com>
2022-08-29 19:07:49 -0400

    Add padding to HIBP response

--- HIBP.bash
+++ HIBP.bash
@@ -7,7 +7,7 @@ else
 	HASH=$(pass shasum $1)
 	HEAD=$(printf $HASH | head -c 5)
 	TAIL=$(printf $HASH | tail -c 35)
-	MATCH=$(curl -s -H "hibp-api-key: $HIBP_KEY" https://api.pwnedpasswords.com/range/$HEAD | grep -i $TAIL)
+	MATCH=$(curl -s -H "hibp-api-key: $HIBP_KEY" -H "Add-Padding: true" https://api.pwnedpasswords.com/range/$HEAD | grep -i $TAIL)
 	[[ -n $MATCH ]] && echo "$1 has been pwned!"
 fi
 
